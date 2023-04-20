d58bede222090608bf506e0381cb556570383adb
MYGelo(MiholapOleg) <oleg.miholap95@gmail.com>
2023-04-21 01:29:13 +0300

    LOader

--- src/components/loader/loader.js
+++ src/components/loader/loader.js
@@ -1,8 +1,18 @@
+import { ProgressBar } from 'react-loader-spinner';
+
 export const Loader = () => {
   console.log(`loader!`);
   return (
     <div>
-      <p>loading...</p>
+      <ProgressBar
+        height="80"
+        width="80"
+        ariaLabel="progress-bar-loading"
+        wrapperStyle={{}}
+        wrapperClass="progress-bar-wrapper"
+        borderColor="#F4442E"
+        barColor="#51E5FF"
+      />
     </div>
   );
 };
